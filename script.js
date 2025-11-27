c870371e64bc84da6b2e60b7f645c70852105263
harpoonwithaz <otad19@gmail.com>
2025-11-27 03:19:56 -0800

    Added collapseabels, made responsive for mobile

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 let theme = localStorage.getItem('light-mode')
 const themeSwitch = document.getElementById('theme-switch')
+var coll = document.getElementsByClassName("collapsible");
+
+// open all by default
+for (let i = 0; i < coll.length; i++) {
+  let content = coll[i].nextElementSibling;
+  content.style.maxHeight = content.scrollHeight + "px";
+}
+
+// toggle behavior
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;   // collapse
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px"; // expand
+    }
+  });
+}
+
 
 const enableLightMode = () => {
     document.body.classList.add('light-mode')
